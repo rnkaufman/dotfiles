6a9afdeeaa52b14229723e5cfad9bbf40fabd230
rnkaufman <rnkaufman@github.com>
2022-03-17 15:56:21 -0700

    Always install oh-my-zsh

--- install.sh
+++ install.sh
@@ -10,7 +10,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall > /dev/null
 
-# install ohmyzsh if not in a codespace
-if [ $CODESPACES != 'true' ] ; then
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
