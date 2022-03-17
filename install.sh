4606e7217960a09594924d29dd74efedc8cb6249
rnkaufman <rnkaufman@github.com>
2022-03-17 15:53:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 # symlink dotfiles
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
+ln -sf $(pwd)/.zshrc ~/.zshrc
 
 # install vim-plug and install plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall > /dev/null
+
+# install ohmyzsh if not in a codespace
+if [ $CODESPACES != 'true' ] ; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
